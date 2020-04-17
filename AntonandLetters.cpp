0de5d48575168b3237ecdbf080ce0cf92cdf6431
Nikhil569 <48991799+Nikhil569@users.noreply.github.com>
2020-04-17 17:32:43 +0530

    Add files via upload

---
+++ AntonandLetters.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	string str;
+	getline(cin,str);
+	int a = str.size();
+	cout<<a<<endl;
+	int counter = 0;
+
+	for(int i=1;i<a-1;i=i+3){
+		if(str[i] != '\0'){
+			for(int j=4;j<a-1;j=j+3){
+				if(str[i]==str[j]) str[j]='\0';
+			}
+
+			counter = counter + 1;
+		}
+	}
+
+	cout<<counter;
+
+
+
+	return 0;
+}
