10f87c74b23306700f3a2b28fe561dd494976819
Nikhil569 <48991799+Nikhil569@users.noreply.github.com>
2020-04-17 17:41:44 +0530

    Update AntonandLetters.cpp

--- AntonandLetters.cpp
+++ AntonandLetters.cpp
@@ -6,12 +6,12 @@ int main(int argc, char const *argv[])
 	string str;
 	getline(cin,str);
 	int a = str.size();
-	cout<<a<<endl;
+	//cout<<a<<endl;
 	int counter = 0;
 
 	for(int i=1;i<a-1;i=i+3){
 		if(str[i] != '\0'){
-			for(int j=4;j<a-1;j=j+3){
+			for(int j=i+3;j<a-1;j=j+3){
 				if(str[i]==str[j]) str[j]='\0';
 			}
 
